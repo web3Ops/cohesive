2b9f353b908e242edd73b7eaba9c33790acd2b5a
Astinz <em.ei@oktuss.com>
2022-03-03 18:47:04 +0300

    wire frontend...

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QtQuick/QQuickWindow>
 
-int main() {
-    std::cout << "Solana GUI..." << std::endl;
-    return 0;
+
+int main(int argc, char *argv[]) {
+
+    QGuiApplication app(argc, argv);
+
+    QQuickWindow::setSceneGraphBackend("software");
+
+    QQmlApplicationEngine engine;
+
+    const QUrl url(u"qrc:/src/core/res/ui/main.qml"_qs);
+
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+                if (!obj && url == objUrl)
+                    QCoreApplication::exit(-1);
+            }, Qt::QueuedConnection);
+
+    engine.load(url);
+
+    return app.exec();
 }
